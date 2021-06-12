d9b2c3f564d4797b4fb4b2e700db99c20fc12fd8
chasscepts <francischassobetta@gmail.com>
2021-06-12 11:02:12 +0100

    Implement Utils.toBits

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,43 @@
 /* eslint-disable no-bitwise */
 
-const lsb = (num) => [num & 0xFF, num >> 8];
+const lsb = (number) => [number & 0xFF, number >> 8];
+
+/**
+ * Converts a number to an array of length 8.
+ * Each item in the array is set to true if corresponding bit
+ * in binary representation of number is 1 and false otherwise.
+ * @param {Number} number to convert
+ * @returns an array of length 8
+ */
+const toBits = (number) => {
+  if (number < 0 || number > 255) {
+    throw new Error('Numner must be in the range (1, 255) inclusive');
+  }
+  const bits = [
+    !!(number & 128),
+    !!(number & 64),
+    !!(number & 32),
+    !!(number & 16),
+    !!(number & 8),
+    !!(number & 4),
+    !!(number & 2),
+    !!(number & 1),
+  ];
+
+  const isset = (pos) => {
+    if (pos < 0 || pos > 7) {
+      return false;
+    }
+    return bits[pos];
+  };
+
+  return {
+    bits,
+    isset,
+  };
+};
 
 export default {
   lsb,
+  toBits,
 };
