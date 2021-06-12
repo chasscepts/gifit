436d73f5a223ebbca0e26bc387ab6f761850ba10
chasscepts <francischassobetta@gmail.com>
2021-06-13 00:22:00 +0100

    Implement utils::logicalScreenDescriptor

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,13 @@
 /* eslint-disable no-bitwise */
 
+const EXTENSION_INTRODUCER = 0x21;
+
+const GRAPHIC_CONTROL_LABEL = 0xF9;
+
+const BLOCK_TERMINATOR = 0;
+
+const HEADER = [0x47, 0x49, 0x46, 0x38, 0x39, 0x61]; // GIF89a
+
 const lsb = (number) => [number & 0xFF, number >> 8];
 
 /**
@@ -85,10 +93,67 @@ const byteLength = (ctLength) => {
   return toBinary(N, 3);
 };
 
+const NBytes = (ctLength) => {
+  const getLength = (n) => 2 ** (n + 1);
+  let N = 1;
+  let length = getLength(N);
+  while (length < ctLength) {
+    N += 1;
+    length = getLength(N);
+    if (length > ctLength) {
+      throw new Error('color table length MUST be a power of 2.');
+    }
+  }
+
+  return toBinary(N, 3);
+};
+
+const uint8ToBase64String = (data) => {
+  const CHUNK_SIZE = 0x8000;
+  const c = [];
+  for (let i = 0; i < data.length; i += CHUNK_SIZE) {
+    c.push(String.fromCharCode.apply(null, data.slice(i, i + CHUNK_SIZE)));
+  }
+
+  return btoa(c.join(''));
+};
+
+const logicalScreenDescriptor = (options = {
+  width: 0, height: 0, resolution: 0, sorted: false, ctLength: 0,
+}) => {
+  const rslt = [...lsb(options.width), ...lsb(options.height)];
+  const ctBits = [0, 0, 0, 0, 0, 0, 0, 0];
+  if (options.ctLength) {
+    const byteLength = NBytes(options.ctLength);
+    let pos = 0;
+    ctBits[pos] = 1;
+    pos += 1;
+    const resolution = options.resolution ? toBinary(options.resolution - 1, 3) : byteLength;
+    for (let i = 0; i < resolution.length; i += 1) {
+      ctBits[pos] = resolution[i];
+      pos += 1;
+    }
+    if (options.sorted) {
+      ctBits[pos] = 1;
+    }
+    pos += 1;
+    for (let i = 0; i < resolution.length; i += 1) {
+      ctBits[pos] = byteLength[i];
+      pos += 1;
+    }
+  }
+  rslt.push(fromBits(ctBits), 0, 0);
+
+  return rslt;
+};
+
 export default {
+  HEADER,
   lsb,
   toBits,
   fromBits,
   toBinary,
   byteLength,
+  uint8ToBase64String,
+  logicalScreenDescriptor,
 };
